1cedd20a5e42aa7c1445e1a1e8ffbc82d90d52e6
Tom Karpeichik <tkarpeichik@tendrilinc.com>
2013-04-06 15:47:38 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FabriqDemo::Application.config.secret_token = '3af6ec30d6a81c07fb5a403998baf1461e8d3781c8221880884cba3e58bc35fa09cdc71c31b6132ca7f5f1185cf7e0908ebabe1554e4aab28b3107003e80f1f9'
